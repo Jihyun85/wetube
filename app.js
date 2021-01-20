84e6d12c8dcc8aae4eaea073b036c722dbfa74e7
Jihyun <hwiyu25@gmail.com>
2021-01-20 11:31:32 +0900

    #2.8 router

--- app.js
+++ app.js
@@ -4,13 +4,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => {
   res.send("Hello from him");
@@ -18,19 +14,17 @@ const handleHome = (req, res) => {
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-app.use(cookieParser);
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(helmet());
 app.use(morgan("dev"));
 
-const middleWare = (req, res, next) => {
-  res.send(`not happening`);
-};
-
-app.get("/", middleWare, handleHome);
+app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
