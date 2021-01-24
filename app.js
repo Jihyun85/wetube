f6763e9236243802ca472ffd724d391f0c543d99
Jihyun <hwiyu25@gmail.com>
2021-01-24 22:01:42 +0900

    #3.4 - #3.5 MongoDB Schema, Model

--- app.js
+++ app.js
@@ -13,12 +13,6 @@ import routes from "./routes";
 const app = express();
 
 app.use(helmet()); // 안전을 위해 미들웨어 중 가장 위에 둔다
-app.set("view engine", "pug");
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("dev"));
-
 app.use(function (req, res, next) {
   res.setHeader(
     "Content-Security-Policy",
@@ -26,6 +20,11 @@ app.use(function (req, res, next) {
   );
   return next();
 });
+app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 app.use(localsMiddleware);
 app.use(routes.home, globalRouter);
