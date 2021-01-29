316f735449e6c0e2016b0e2de733f99a8f11bc28
Jihyun <hwiyu25@gmail.com>
2021-01-29 10:26:42 +0900

    #4.3 ES6 with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
+  devtool: "cheap-module-source-map",
   entry: ENTRY_FILE,
   mode: MODE,
   module: {
